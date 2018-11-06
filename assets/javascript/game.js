897803b2f66a5491a9b831faca2c17e98426a532
Vader55 <38086201+barb03@users.noreply.github.com>
2018-11-06 17:58:05 -0600

    added background image, css styles for image, html id name change and added favicon

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,43 @@
-window.onload = function now() {
-    $("#btnSubmit").on('click', function () {
-        searchNow();
-    })
-}
-function searchNow() {
-    var search = $("#search-term").val();
-    console.log(search);
+$("button").on("click", function() {
+  
+  var movie = $(this).attr("data-horror");
+
+  
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  
+    movie + "&api_key=eLxGXhONJQrhCzNyIkNKADsNnRWPulEc";
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
     
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-      'api-key': "191377acaaa14557a7edaa51e7861c61",
-      'q': search,
-      'begin_date': yearStart,
-      'end_date': yearStop,
-      'sort': "newest"
-    });
-    $.ajax({
-      url: url,
-      method: 'GET',
-    }).done(function(result) {
-      console.log(result);
-      for (var i = 0; i < numArticles; i++) {
-        console.log(result.response.docs[i].headline.main);
-        $("#results").append(result.response.docs[i].headline.main + "<br>");
-        $("#results").append(result.response.docs[i].byline.original + "<br>");
+    .then(function(response) {
+      console.log(queryURL);
+
+      console.log(response);
+      
+      var results = response.data;
+
+      
+      for (var i = 0; i < results.length; i++) {
+
+        
+        var horrorDiv = $("<div>");
+
+        
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        
+        var horrorImage = $("<img>");
+        
+        horrorImage.attr("src", results[i].images.fixed_height.url);
+
+        
+        horrorDiv.append(p);
+        horrorDiv.append(horrorImage);
+
+        
+        $("#gifs-appear-here").prepend(horrorDiv);
       }
-
-    }).fail(function(err) {
-      throw err;
     });
-}
-
+});
