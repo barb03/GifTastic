df3b0e5966a2830a09490452285868448ca2f6d2
Vader55 <38086201+barb03@users.noreply.github.com>
2018-11-06 18:35:05 -0600

    added function for still and animate gifs in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
-$("button").on("click", function() {
-  
-  var movie = $(this).attr("data-horror");
 
-  
+
+
+$("button").on("click", function() {  
+  $('#GIFArea').empty();
+  var movie = $(this).attr("data-horror");  
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  
     movie + "&api_key=eLxGXhONJQrhCzNyIkNKADsNnRWPulEc";
   
@@ -11,33 +12,35 @@ $("button").on("click", function() {
     method: "GET"
   })
     
-    .then(function(response) {
-      console.log(queryURL);
-
-      console.log(response);
-      
-      var results = response.data;
-
-      
-      for (var i = 0; i < results.length; i++) {
-
+  .then(function(response) {        
+    var results = response.data;   
+    for (var i = 0; i < results.length; i++) {        
+      var horrorDiv = $("<div>");        
+      var p = $("<p>").text("Rating: " + results[i].rating);   
+      var horrorImage = $("<img>");
         
-        var horrorDiv = $("<div>");
-
-        
-        var p = $("<p>").text("Rating: " + results[i].rating);
-
-        
-        var horrorImage = $("<img>");
-        
-        horrorImage.attr("src", results[i].images.fixed_height.url);
-
-        
+      horrorImage.attr('src', results[i].images.fixed_height_still.url)
+        .attr('data-still', results[i].images.fixed_height_still.url)
+	      .attr('data-animate', results[i].images.fixed_height.url)
+	      .attr('data-state', "still")
+        .addClass("showImage");  
+                       
         horrorDiv.append(p);
-        horrorDiv.append(horrorImage);
-
-        
+        horrorDiv.append(horrorImage);        
         $("#gifs-appear-here").prepend(horrorDiv);
-      }
-    });
+    }
+  });    
 });
+
+$(document).on('click', '.showImage',  function() {
+  var state = $(this).data('state');
+  if (state == "still") {      
+      $(this).attr('src', $(this).data('animate'))
+      .data('state', 'animate');
+  } 
+  else {      
+      $(this).attr('src', $(this).data('still'))
+      .data('state', 'still');               
+  }
+});
+
