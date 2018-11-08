dabcff186575f2f4e78177c3582df3935048d4f9
Vader55 <38086201+barb03@users.noreply.github.com>
2018-11-08 09:48:43 -0600

    added comments for files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var movies = ["It", "Frankenstein", "Dracula"];
   GIFArea = " ";  
   
-  function renderButtons() {  
+  function renderButtons() {                        //function to add movie buttons
     $("#movies-view").empty(); 
     for (var i = 0; i < movies.length; i++) {  
     var a = $("<button>");  
@@ -17,31 +17,28 @@ $(document).ready(function(){
   
   renderButtons();  
     
-    $("#add-movie").on("click", function(event) {  
-      event.preventDefault(); 
+    $("#add-movie").on("click", function(event) {   //function to add buttons for textbox submits
+      event.preventDefault();                       
       var movie = $("#movie-input").val().trim();
-      if(movie != ""){             
-      movies.push(movie);
-      console.log(movie)  
+      if(movie != ""){                              //to disable the submit button if no text in textbox
+      movies.push(movie);        
       renderButtons(); 
       } //end if
   });  //end add movie
   
   
-  $(document).on("click", "button",  function() {     
+  $(document).on("click", "button",  function() {    //function to add the gifs to page
     $("#GIFArea").empty(); 
     var b = $(this).attr("data-name");		
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  //giphy api and key
     b + "&api_key=eLxGXhONJQrhCzNyIkNKADsNnRWPulEc"; 
-
-    console.log(queryURL);            
-    $.ajax ({
+           
+    $.ajax ({                                        //ajax to get gifs
     url: queryURL,
     method: "GET"
     })  //end ajax
             
-    .done(function(response) {
-      console.log(response);         
+    .done(function(response) {                       //function to make the gifs still until clicked on     
       var results = response.data;            
       for (var i = 0; i < results.length; i++) {           
         var gifDiv = $("<div class='item'>");           
@@ -60,15 +57,13 @@ $(document).ready(function(){
     });  //end function response
   });  //end on click buttons
   
-  $(document).on("click", ".showImage",  function() {  
+  $(document).on("click", ".showImage",  function() {   //function to animate the gifs when clicked on
     var state = $(this).data("state");      
-    if (state == "still") {
-      console.log("still image works");         
+    if (state == "still") {               
       $(this).attr("src", $(this).data("animate"))
       .data("state", "animate");
     } //end if
-    else {        
-      console.log("animated image works");
+    else {              
       $(this).attr("src", $(this).data("still"))
       .data("state", "still");               
     }  //end else
